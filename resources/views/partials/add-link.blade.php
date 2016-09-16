f5b8d5d47170ad4e68a0c816542517a16a584150
2Lev <georgetulev@abv.bg>
2016-09-16 15:36:39 +0300

    Initial!

---
+++ resources/views/partials/add-link.blade.php
@@ -0,0 +1,46 @@
+@if(Auth::check())
+<div class="col-md-4">
+    <h3>Contribute Link</h3>
+
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <form method="POST" action="/community">
+                {{ csrf_field() }}
+
+                <div class="form-group {{ $errors->has('channel_id') ? 'has-error' : '' }}">
+                    <label for="Channel">Channel</label>
+                    <select class="form-control" name="channel_id">
+                            <option selected disabled>Pick a channel...</option>
+                        @foreach($channels as $channel)
+                            <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                {{ $channel->title }}
+                            </option>
+                        @endforeach
+                    </select>
+
+                    {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
+                </div>
+
+                <div class="form-group  {{ $errors->has('title') ? 'has_error' : '' }}">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" value="{{ old('title') }}" id="title" name="title" placeholder="What is the title for your article?" required>
+                    {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
+                </div>
+
+                <div class="form-group {{ $errors->has('link') ? 'has_error' : '' }}">
+                    <label for="link">Link:</label>
+                    <input type="text" class="form-control" value="{{ old('link') }}" id="link" name="link" placeholder="What is the URL?" required>
+                    {!! $errors->first('link', '<span class="help-block">:message</span>') !!}
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary">Contribute Link</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+@else
+    <h3>Please sign in ...</h3>
+@endif
